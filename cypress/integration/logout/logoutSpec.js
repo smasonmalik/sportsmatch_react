1a3cfdc0a1500133acba385b1c832af044a91040
Duncan Skinner <duncanskinner94@gmail.com>
2019-11-28 16:48:59 +0000

    user can logout

---
+++ cypress/integration/logout/logoutSpec.js
@@ -0,0 +1,7 @@
+describe("logout", function() {
+  it("successfully logs user out", function() {
+    cy.login()
+    cy.get('#logout-button').click()
+    cy.url().should('eq', 'http://localhost:3000/login')
+  })
+})
