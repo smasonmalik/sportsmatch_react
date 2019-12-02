9d8ea2814597a1fab37977827682fed9d286b4c4
Jessica Marais <jessica_marais@hotmail.com>
2019-12-02 11:26:16 +0000

    Add CreateMessage component

--- src/components/DisplayMessages.jsx
+++ src/components/DisplayMessages.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import { Redirect } from 'react-router-dom';
 import axios from "axios";
+import CreateMessage from './CreateMessage';
 
 class DisplayMessages extends Component {
   constructor(props) {
