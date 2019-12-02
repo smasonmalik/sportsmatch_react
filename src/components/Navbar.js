c39d8a6b0750f70df07ab125dfd8ad96c90a4da6
Sid Mason-Malik <49129863+smasonmalik@users.noreply.github.com>
2019-12-02 18:28:35 +0000

    routing updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -31,7 +31,7 @@ class Navbar extends React.Component {
 
     return (
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <NavLink className="navbar-brand" to="/home"><img className='main-logo' src="../../sportsmatchlogo.png" alt='SportsMatch'/></NavLink>
+        <NavLink className="navbar-brand" to="/"><img className='main-logo' src="../../sportsmatchlogo.png" alt='SportsMatch'/></NavLink>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
