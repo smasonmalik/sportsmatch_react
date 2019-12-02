70136848f95661ac9e8fa26094318f96b7d908cb
Mezela <p_mezue@hotmail.com>
2019-12-02 12:38:21 +0000

    Split games on results page by games organised and games visited

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,20 +1,24 @@
 import React from 'react'
 import axios from 'axios'
 import SingleResult from './SingleResult'
+import './css/Results.css'
 
 class Results extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      results: [],
+      opponentResults: [],
+      organiserResults: [],
       resultEdit: false
     }
-    this.getRequest = this.getRequest.bind(this)
+    this.getOpponentResultsRequest = this.getOpponentResultsRequest.bind(this)
+    this.getOrganiserResultsRequest = this.getOrganiserResultsRequest.bind(this)
     this.handleEdit = this.handleEdit.bind(this)
   }
 
   componentDidMount() {
-    this.getRequest()
+    this.getOpponentResultsRequest()
+    this.getOrganiserResultsRequest()
   };
 
   componentDidUpdate(prevProps, prevState) {
@@ -23,10 +27,10 @@ class Results extends React.Component {
     }
   }
 
-  getRequest() {
+  getOpponentResultsRequest() {
     let self = this;
     axios({
-      url: "/api/v1/results/",
+      url: "/api/v1/results/opponent",
       headers: {
         "Content-Type": "application/json",
         "api-token": localStorage.getItem('jwtToken')
@@ -34,7 +38,27 @@ class Results extends React.Component {
     })
       .then(function(response) {
         self.setState({
-          results: response.data
+          opponentResults: response.data
+        })
+        console.log(response.data)
+      })
+      .catch(function(error) {
+        console.log(error)
+      })
+  }
+
+  getOrganiserResultsRequest() {
+    let self = this;
+    axios({
+      url: "/api/v1/results/organiser",
+      headers: {
+        "Content-Type": "application/json",
+        "api-token": localStorage.getItem('jwtToken')
+      }
+    })
+      .then(function(response) {
+        self.setState({
+          organiserResults: response.data
         })
         console.log(response.data)
       })
@@ -52,20 +76,43 @@ class Results extends React.Component {
   render() {
       return (
       <div>
-      <h4>My Results</h4>
-      <ul className="list-group list-group-flush">
-        {this.state.results.map(result => (
-          <SingleResult
-            key={result.id}
-            id={result.id}
-            game_id={result.game_id}
-            winner_id={result.winner_id}
-            loser_id={result.loser_id}
-            confirmed={result.confirmed}
-            handleEdit={this.handleEdit}
-          />
-        ))}
-      </ul>
+        <h2 align="center">My Results</h2>
+        <div class="container">
+          <div class="row">
+            <div class="col-sm">
+            <h3>I'm the organiser</h3>
+            <ul className="list-group list-group-flush">
+              {this.state.organiserResults.map(result => (
+              <SingleResult
+                key={result.id}
+                id={result.id}
+                game_id={result.game_id}
+                winner_id={result.winner_id}
+                loser_id={result.loser_id}
+                confirmed={result.confirmed}
+                handleEdit={this.handleEdit}
+              />
+              ))}
+            </ul>
+            </div>
+            <div class="col-sm">
+            <h3>I'm the opponent</h3>
+              <ul className="list-group list-group-flush">
+                {this.state.opponentResults.map(result => (
+                  <SingleResult
+                    key={result.id}
+                    id={result.id}
+                    game_id={result.game_id}
+                    winner_id={result.winner_id}
+                    loser_id={result.loser_id}
+                    confirmed={result.confirmed}
+                    handleEdit={this.handleEdit}
+                  />
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
       )
     }
