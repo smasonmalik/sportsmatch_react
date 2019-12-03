079aca30a5f37d4c5d7cc57d503e0f00388eb560
Duncan Skinner <duncanskinner94@gmail.com>
2019-12-03 21:03:58 +0000

    styling login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { NavLink, Redirect } from "react-router-dom";
 import axios from "axios";
 import Home from './Home'
+import './css/login.css'
 
 
 class Login extends Component {
@@ -40,51 +41,56 @@ class Login extends Component {
       return <Home />;
     } else {
       return (
-        <div className="form-container">
-          <h4>Log in</h4>
-          <form
-            onSubmit={e => {
-              e.preventDefault();
-            }}
-            className="form-login"
-          >
-            <div className="form-group">
-              <input
-                id="email-input"
-                name="email"
-                placeholder="email"
-                type="text"
-                required="required"
-                className="email form-control"
-              ></input>
-            </div>
-            <div className="form-group">
-              <input
-                id="password-input"
-                name="password"
-                placeholder="password"
-                type="password"
-                className="password form-control"
-                required="required"
-              ></input>
-            </div>
-            <div className='row'>
-              <div className='col'>
-              <div className="form-group">
-              <button
-                name="login"
-                type="submit"
-                className="login-button btn btn-primary"
-                onClick={this.handleLogin}>
-                Login
-              </button>
-            </div>
+        <div className="container my-container">
+          <div className="row my-row">
+            <div className="col-4 my-col" id="login_form">
+              <div className="form-container">
+                <img className='main-logo' src="../../sportsmatchlogo.png" alt='SportsMatch'/>
+                <h5>Welcome Back, Please Login to Your Account</h5>
+                <form
+                  onSubmit={e => {
+                    e.preventDefault();
+                  }}
+                  className="form-login"
+                >
+                  <div>
+                    <label>email</label><br/>
+                    <input
+                      id="email-input"
+                      name="email"
+                      type="text"
+                      required="required"
+                      class="no-outline"
+                    />
+                  </div>
+                  <div>
+                    <label>password</label><br/>
+                    <input
+                      id="password-input"
+                      name="password"
+                      type="password"
+                      required="required"
+                      class="no-outline"
+                    />
+                  </div>
+                  <div className="form-group" style={{textAlign: 'center'}}>
+                    <button
+                      name="login"
+                      type="submit"
+                      className="login-button"
+                      onClick={this.handleLogin}>
+                      Login
+                    </button>
+                  </div>
+                </form>
+                <div style={{textAlign: 'center'}}>
+                  <NavLink to='/signup'>Sign Up</NavLink>
+                </div>
+                </div>
               </div>
+              <div className="col-8" id="right-column"></div>
             </div>
-
-          </form>
-          <NavLink to='/signup'>Sign Up</NavLink>
-        </div>
+          </div>
       );
     }
   }
