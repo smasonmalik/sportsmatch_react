5c66d722efd418d83e5a0c566d81b6f613228afc
Sid Mason-Malik <49129863+smasonmalik@users.noreply.github.com>
2019-12-03 14:35:02 +0000

    opponent name displayed on game requests
    Co-Authored-By: Vijay Kurian <kurianvijay7@gmail.com>

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -15,7 +15,7 @@ class SearchBar extends React.Component {
             <div className="row">
                 <div className="col">
 
-                    <label className="label">Filter By F.R.E.D. Ability:</label>
+                    <label className="label">Filter By F.R.E.D (Ability):</label>
                     <select className="custom-select" name="ability" id="ability-select" onChange={(e) => this.props.handleChange(e)}>
                         <option value="Beginner">Beginner</option>
                         <option value="Intermediate">Intermediate</option>
--- src/components/SingleGameRequest.jsx
+++ src/components/SingleGameRequest.jsx
@@ -21,8 +21,7 @@ class Request extends React.Component {
       <li className="list-group-item">
         <div className="card" style={{width: '18rem'}}>
           <div className="card-body">
-              <p className="card-text">Organiser id: {this.props.organiser_id}</p>
-              <p className="card-text">Opponent id: {this.props.opponent_id}</p>
+              <p className="card-text">Opponent: {this.props.opponent_name}</p>
               <p className="card-text">Game Date: {this.props.game_date}</p>
               <p className="card-text">Game Time: {this.props.game_time}</p>
               <ConfirmGameButton
