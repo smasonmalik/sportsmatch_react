b0a8fd4c7b086559c03a1b217e5760d5c645a592
Jessica Marais <jessica_marais@hotmail.com>
2019-12-03 13:51:12 +0000

    Only organiser can add result
    Co-Authored-By: Mezela <p_mezue@hotmail.com>

--- src/components/SingleResult.jsx
+++ src/components/SingleResult.jsx
@@ -6,24 +6,39 @@ import NewResult from "./NewResult"
 class SingleResult extends React.Component {
 
   render() {
-
-    return (
-      <li className="list-group-item">
-        <div className="card" style={{width: '18rem'}}>
-          <div className="card-body">
-              <p className="card-text">Game id: {this.props.id}</p>
-              <p className="card-text">Winner id: {this.props.winner_id}</p>
-              <p className="card-text">Loser id: {this.props.loser_id}</p>
-              <p className="card-text">Organiser ID: {this.props.organiser_id}</p>
-              <p className="card-text">Opponent ID: {this.props.opponent_id}</p>
-              <p className="card-text">Game Date: {this.props.game_date}</p>
-
-              <Link to={`/results/${this.props.id}/new`} className="btn custom-button">
-              Create New Results</Link>
+    if (localStorage.getItem('user_id') == this.props.organiser_id) {
+      return (
+        <li className="list-group-item">
+          <div className="card" style={{width: '18rem'}}>
+            <div className="card-body">
+                <p className="card-text">Game id: {this.props.id}</p>
+                <p className="card-text">Winner id: {this.props.winner_id}</p>
+                <p className="card-text">Loser id: {this.props.loser_id}</p>
+                <p className="card-text">Organiser ID: {this.props.organiser_id}</p>
+                <p className="card-text">Opponent ID: {this.props.opponent_id}</p>
+                <p className="card-text">Game Date: {this.props.game_date}</p>
+                <Link to={`/results/${this.props.id}/new`} className="btn custom-button">
+                Create New Result</Link>
+            </div>
           </div>
-        </div>
-      </li>
-    )
+        </li>
+      )
+    } else {
+      return (
+        <li className="list-group-item">
+          <div className="card" style={{width: '18rem'}}>
+            <div className="card-body">
+                <p className="card-text">Game id: {this.props.id}</p>
+                <p className="card-text">Winner id: {this.props.winner_id}</p>
+                <p className="card-text">Loser id: {this.props.loser_id}</p>
+                <p className="card-text">Organiser ID: {this.props.organiser_id}</p>
+                <p className="card-text">Opponent ID: {this.props.opponent_id}</p>
+                <p className="card-text">Game Date: {this.props.game_date}</p>
+            </div>
+          </div>
+        </li>
+      )
+    }
   }
 }
 export default SingleResult
