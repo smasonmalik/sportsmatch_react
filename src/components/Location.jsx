f05196492523fcfc2e62f0acc7beb25028bc1311
Vijay Kurian <kurianvijay7@gmail.com>
2019-12-03 14:12:01 +0000

    Initilized Location component

---
+++ src/components/Location.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
+
+class Location extends Component {
+  constructor(props) {
+    super(props)
+
+  }
+}
+
+export default Location
