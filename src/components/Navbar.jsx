b8e201c451bc2041273ecab5801bd21cc1aa03b9
domtunstill <datunstill@gmail.com>
2019-12-03 17:23:27 +0000

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,41 +28,6 @@ class Navbar extends React.Component {
     }
 
   render() {
-<<<<<<< HEAD:src/components/Navbar.js
-    if (localStorage.getItem('jwtToken')) {
-      return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <NavLink className="navbar-brand" to="/home">SportsMatch</NavLink>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item active">
-                <NavLink className="nav-link" to="/home">Home <span className="sr-only">(current)</span></NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" id="login-link" to="/login">Login</NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" id="profile-link" to='/profile'>Profile</NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" id="results-link" to='/results'>Results</NavLink>
-              </li>
-              <LogoutButton handleLogout={this.props.handleLogout} />
-  
-            </ul>
-          </div>
-        </nav>
-      )
-    } else {
-      return (
-        <div>
-          <h2>
-          Welcome to SportsMatch!
-          </h2>
-=======
 
     return (
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
@@ -78,9 +43,11 @@ class Navbar extends React.Component {
             <li className="nav-item">
               <NavLink className="nav-link" id="profile-link" to='/profile'>Profile</NavLink>
             </li>
+            <li className="nav-item">
+                <NavLink className="nav-link" id="results-link" to='/results'>Results</NavLink>
+            </li>
             {this.sessionButton()}
           </ul>
->>>>>>> master:src/components/Navbar.jsx
         </div>
       )
     }
