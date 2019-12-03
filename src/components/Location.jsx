f7cd00ddc379d75ba31cab7fc3fba5ee818d07fc
Vijay Kurian <kurianvijay7@gmail.com>
2019-12-03 14:26:44 +0000

    Add map component to render function

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Redirect } from 'react-router-dom'
 
 class Location extends Component {
   constructor(props) {
     super(props)
 
+  }
+
+  render() {
+    return  (
+      <Map
+        google={this.props.google}
+        zoom={8}
+        style={mapStyles}
+        initialCenter={{ lat: 47.444, lng: -122.176}}
+       >
+       <Marker position={{ lat: 48.00, lng: -122.00}} />
+        </Map>
+    )
   }
 }
 
