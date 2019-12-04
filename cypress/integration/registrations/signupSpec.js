aa58526f4f9eef75bb6a61aa781d428582a00615
Dom Tunstill <datunstill@gmail.com>
2019-12-04 22:30:53 +0000

    setup default routes and json files

--- cypress/integration/registrations/signupSpec.js
+++ cypress/integration/registrations/signupSpec.js
@@ -1,15 +1,19 @@
 describe('signup', function() {
   it('user can successfully sign up', function() {
-    cy.server()           // enable response stubbing
-    cy.route({
-      method: 'POST',      // Route all POST requests
-      url: '/api/v1/players/new',    // that have a URL that matches '/users/*'
-      response: {
-        "jwt_token": "iamatoken",
-        "user_id": 10
-    }        // and force the response to be: []
-    })
-    cy.signup()
+    cy.visit('/signup')
+
+    cy.get('#first-name-input').type('D')
+    cy.get('#last-name-input').type('T')
+    cy.get('#dob-input').type('1991-01-01')
+    cy.get('#ability-input').select('Beginner')
+    cy.get('#postcode-input').type('se233ly')
+    cy.get('#gender-input').select('Male')
+    cy.get('#sport-input').select('Tennis')
+    cy.get('#email-input').type('dom@dom.com')
+    cy.get('#password-input').type('password')
+    cy.get('#password-confirm-input').type('password')
+  
+    cy.get('.signup-button').click()
     cy.url().should('eq', 'http://localhost:3000/profile')
   })
 })
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -15,6 +15,21 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+// import './response'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+beforeEach(function () {
+    cy.server()  
+    cy.route({
+        method: 'POST',      // Route all POST requests
+        url: '/api/v1/players/new',    // that have a URL that matches '/users/*'
+        response: 'fixtures:login_signup'       // and force the response to be: []
+    })
+    cy.route({
+        method: 'GET',      // Route all GET requests
+        url: '/api/v1/players/NaN',    // that have a URL that matches '/users/*'
+        response: 'fixtures:current_player'      // and force the response to be: []
+    })
+  })
