3ccd2a53775e9a12ce0b15460820b8bcba506e51
domtunstill <datunstill@gmail.com>
2019-12-04 17:39:30 +0000

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
-import SearchBar from './FilterBar';
+import FilterBar from './FilterBar';
 import Player from './Player';
 import Login from './Login'
 
