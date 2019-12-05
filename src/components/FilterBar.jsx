816bfbf28ff273a7256bc8c06bc9d6ae4600184e
Sid Mason-Malik <49129863+smasonmalik@users.noreply.github.com>
2019-12-05 18:27:30 +0000

    remove FRED from filter

--- src/components/FilterBar.jsx
+++ src/components/FilterBar.jsx
@@ -18,7 +18,7 @@ class FilterBar extends React.Component {
         <form>
             <div className="row justify-content-between">
                 <div className={`col-lg-5 col-md-5 col-sm-12 ${styles.col}`}>
-                    <label className={`label ${styles.label}`}>Ability(F.R.E.D.)</label>
+                    <label className={`label ${styles.label}`}>Ability</label>
                     <select className="custom-select" name="ability" id="ability-select" onChange={(e) => this.props.handleChange(e)}>
                         <option value={this.props.ability}>{this.props.ability}</option>
                         <option value="Beginner">Beginner</option>
