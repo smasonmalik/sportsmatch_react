0fee5788ddf0de3d1ab248d2ad7ffda9f78768ea
Duncan Skinner <duncanskinner94@gmail.com>
2019-12-06 02:30:22 +0000

    styling signupform

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -96,158 +96,182 @@ class Signup extends Component {
     }
     else {
       return (
-      <div className={`container ${styles.myContainer}`}>
-        <div className="form-container">
-            <h5 className={styles.heading}>Welcome to SportsMatch!<br></br> Create Your Account Here </h5>
-            <form
-              onSubmit={e => {e.preventDefault();}}
-              className="form-signup"
-            >
-              <div>
-                <label>First name</label><br />
-                <input
-                  id="first-name-input"
-                  name="first-name"
-                  type="text"
-                  required="required"
-                  className={styles.inputField}
-                ></input>
+      <div className={styles.topDiv}>
+        <div className={`container ${styles.myContainer}`}>
+          <div className="form-container">
+            <div className='row'>
+              <div className='col-3'></div>
+              <div className={`col-6 ${styles.headingDiv}`}>
+                <h2 className={styles.heading}>Welcome to SportsMatch!<br></br> Create Your Account Here </h2>
               </div>
-              <div>
-                <label>Last name</label><br />
-                <input
-                  id="last-name-input"
-                  name="last-name"
-                  type="text"
-                  required="required"
-                  className={styles.inputField}
-                ></input>
+              <div className='col-3'></div>
+            </div>
+              <form
+                onSubmit={e => {e.preventDefault();}}
+                className="form-signup"
+              >
+              <div className="row">
+                <div className='col-6'>
+                  <label>First name</label><br />
+                  <input
+                    id="first-name-input"
+                    name="first-name"
+                    placeholder="First name"
+                    type="text"
+                    required="required"
+                    className={styles.inputField}
+                  ></input>
+                </div>
+                <div className='col-6'>
+                  <label>Last name</label><br />
+                  <input
+                    id="last-name-input"
+                    name="last-name"
+                    placeholder="Last name"
+                    type="text"
+                    required="required"
+                    className={styles.inputField}
+                  ></input>
+                </div>
               </div>
-              <div className="form-group">
-                <label>Date of Birth</label>
-                <input
-                  id="dob-input"
-                  name="dob"
-                  type="date"
-                  max="2003-12-06"
-                  required="required"
-                  className="form-control"
-                  onChange={e => this.dobValidation(e)}
-                ></input>
-              </div>
-              <div className="form-group">
-                <label>Sport</label><br />
-                <select
-                  id="sport-input"
-                  name="sport"
-                  placeholder="Sport"
-                  type="text"
-                  required="required"
-                  className="form-control"
-                >
-                  <option value="Tennis">-----select sport-----</option>
-                  <option value="Tennis">Tennis</option>
-                  <option value="TableTennis">TableTennis</option>
-                  <option value="Squash">Squash</option>
-                  <option value="Badminton">Badminton</option>
-                </select>
-              </div>
-              <div className="form-group">
-                <label>Ability</label><br />
-                <select
-                  id="ability-input"
-                  name="ability"
-                  placeholder="Ability"
-                  type="select"
-                  required="required"
-                  className="form-control"
-                >
-                  <option value="Beginner">-----select ability-----</option>
-                  <option value="Beginner">Beginner</option>
-                  <option value="Intermediate">Intermediate</option>
-                  <option value="Advanced">Advanced</option>
-                </select>
-              </div>
-              <div className="form-group">
-                <label>Gender</label><br />
-                <select
-                  id="gender-input"
-                  name="gender"
-                  placeholder="Gender"
-                  type="text"
-                  required="required"
-                  className="form-control"
-                >
-                  <option value="Male">-----select gender-----</option>
-                  <option value="Male">Male</option>
-                  <option value="Female">Female</option>
-                  <option value="not_say">Rather Not Say</option>
-                </select>
-              </div>
-              <div>
-                <label>Email</label><br />
-                <input
-                  id="email-input"
-                  name="email"
-                  type="email"
-                  required="required"
-                  className={styles.inputField}
-                  onChange={e => this.validateEmail(e)}
-                ></input>
-              </div>
-              <br/>
-              <div>
-                <label>Postcode</label><br />
-                <input
-                  id="postcode-input"
-                  name="postcode"
-                  type="text"
-                  required="required"
-                  className={styles.inputField}
-                ></input>
-              </div>
-              <div>
-                <label>Password</label><br />
-                <input
-                  id="password-input"
-                  name="password"
-                  type="password"
-                  className={`password ${styles.inputField}`}
-                  required="required"
-                ></input>
-              </div>
-              <div>
-                <label>Confirm password</label><br />
-                <input
-                  id="password-confirm-input"
-                  name="password-confirm"
-                  type="password"
-                  className={`password ${styles.inputField}`}
-                  required="required"
-                  onChange={e => this.handlePasswordConfirm(e)}
-                ></input>
-                <div className={styles.inputField}>
-                  {this.state.errorMessage ?
-                    <FlashMessage
-                      message={this.state.errorMessage}
-                      type="error"
-                    /> : null }
+              <div className="row">
+                <div className="col-12">
+                  <label>Date of Birth</label>
+                  <input
+                    id="dob-input"
+                    name="dob"
+                    type="date"
+                    max="2003-12-06"
+                    required="required"
+                    className="form-control"
+                    onChange={e => this.dobValidation(e)}
+                  ></input>
                 </div>
               </div>
               <div className='row'>
-                <div className='col'>
-                  <div className="form-group">
-                    <button
-                      name="signup"
-                      type="submit"
-                      className="signup-button"
-                      onClick={this.handleSignup}>
-                      Sign up!
-                    </button>
-                  </div>
+                <div className="col-4">
+                  <label>Sport</label><br />
+                  <select
+                    id="sport-input"
+                    name="sport"
+                    placeholder="Sport"
+                    type="text"
+                    required="required"
+                    className="form-control"
+                  >
+                    <option value="Tennis">-----select sport-----</option>
+                    <option value="Tennis">Tennis</option>
+                    <option value="TableTennis">TableTennis</option>
+                    <option value="Squash">Squash</option>
+                    <option value="Badminton">Badminton</option>
+                  </select>
+                </div>
+                <div className="col-4">
+                  <label>Ability</label><br />
+                  <select
+                    id="ability-input"
+                    name="ability"
+                    placeholder="Ability"
+                    type="select"
+                    required="required"
+                    className="form-control"
+                  >
+                    <option value="Beginner">-----select ability-----</option>
+                    <option value="Beginner">Beginner</option>
+                    <option value="Intermediate">Intermediate</option>
+                    <option value="Advanced">Advanced</option>
+                  </select>
+                </div>
+                <div className="col-4">
+                  <label>Gender</label><br />
+                  <select
+                    id="gender-input"
+                    name="gender"
+                    placeholder="Gender"
+                    type="text"
+                    required="required"
+                    className="form-control"
+                  >
+                    <option value="Male">-----select gender-----</option>
+                    <option value="Male">Male</option>
+                    <option value="Female">Female</option>
+                    <option value="not_say">Rather Not Say</option>
+                  </select>
                 </div>
               </div>
-            </form>
+              <div className='row'>
+                <div className='col-6'>
+                  <label>Email</label><br />
+                  <input
+                    id="email-input"
+                    name="email"
+                    type="email"
+                    placeholder="Email"
+                    required="required"
+                    className={styles.inputField}
+                    onChange={e => this.validateEmail(e)}
+                  ></input>
+                </div>
+                <br/>
+                <div className='col-6'>
+                  <label>Postcode</label><br />
+                  <input
+                    id="postcode-input"
+                    name="postcode"
+                    placeholder="Postcode"
+                    type="text"
+                    required="required"
+                    className={styles.inputField}
+                  ></input>
+                </div>
+              </div>
+                <div>
+                  <label>Password</label><br />
+                  <input
+                    id="password-input"
+                    name="password"
+                    placeholder="Password"
+                    type="password"
+                    className={`password ${styles.inputField}`}
+                    required="required"
+                  ></input>
+                </div>
+                <div>
+                  <label>Confirm password</label><br />
+                  <input
+                    id="password-confirm-input"
+                    name="password-confirm"
+                    placeholder="Confirm Password"
+                    type="password"
+                    className={`password ${styles.inputField}`}
+                    required="required"
+                    onChange={e => this.handlePasswordConfirm(e)}
+                  ></input>
+                  <div className={styles.inputField}>
+                    {this.state.errorMessage ?
+                      <FlashMessage
+                        message={this.state.errorMessage}
+                        type="error"
+                      /> : null }
+                  </div>
+                </div>
+                <div className='row'>
+                  <div className='col-4'></div>
+                  <div className='col-4'>
+                    <div className="form-group">
+                      <button
+                        name="signup"
+                        type="submit"
+                        className={styles.signupButton}
+                        onClick={this.handleSignup}>
+                        Sign up!
+                      </button>
+                    </div>
+                  </div>
+                  <div className='col-4'></div>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
     );
