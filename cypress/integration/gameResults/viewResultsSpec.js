a910615fc0ec0ccb375da13ff2a5e7273e58bd6c
Jessica Marais <jessica_marais@hotmail.com>
2019-12-11 15:33:41 +0000

    Fix viewResults tests
    Co-Authored-By: Dom Tunstill <datunstill@gmail.com>

--- cypress/integration/gameResults/viewResultsSpec.js
+++ cypress/integration/gameResults/viewResultsSpec.js
@@ -2,25 +2,25 @@ describe("viewResults", function() {
   it('organiser can see result of a game', function() {
     cy.login()
     cy.visit('/results')
-    cy.contains('Roger VS. Duncan').should('be.visible');
-    cy.contains('Winner: Roger').should('be.visible');
-    cy.contains('Loser: Duncan').should('be.visible');
+    cy.contains('Roger').should('be.visible');
+    cy.contains('Duncan').should('be.visible');
+    cy.contains('You Won!').should('be.visible');
     cy.url().should('eq', 'http://localhost:3000/results')
   })
 
   it('opponent can see result of a game', function() {
     cy.login()
     cy.visit('/results')
-    cy.contains('Duncan VS. Roger').should('be.visible');
-    cy.contains('Winner: Duncan').should('be.visible');
-    cy.contains('Loser: Roger').should('be.visible');
+    cy.contains('Duncan').should('be.visible');
+    cy.contains('Roger').should('be.visible');
+    cy.contains('You Lost!').should('be.visible');
     cy.url().should('eq', 'http://localhost:3000/results')
   })
 
   it('player cannot see results if not logged in', function() {
     cy.visit('/results')
-    cy.contains('Duncan VS. Roger').should('not.exist');
-    cy.contains('Winner: Duncan').should('not.exist');
-    cy.contains('Loser: Roger').should('not.exist');
+    cy.contains('Duncan').should('not.exist');
+    cy.contains('Roger').should('not.exist');
+    cy.contains('You Lost!').should('not.exist');
   })
 })
